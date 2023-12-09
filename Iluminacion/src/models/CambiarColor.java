448b911cf060822570576124095789393b6b170c
SofiPv <153144429+SofiPv@users.noreply.github.com>
2023-12-08 18:00:41 -0600

    Add files via upload

---
+++ Iluminacion/src/models/CambiarColor.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.awt.Color;
+
+public abstract class CambiarColor {
+
+	public static float getRed(Color color) {
+		return (float) color.getRed() / 255f;
+	}
+
+	public static float getGreen(Color color) {
+		return (float) color.getGreen() / 255f;
+	}
+
+	public static float getBlue(Color color) {
+		return (float) color.getBlue() / 255f;
+	}
+
+	public static float getAlpha(Color color) {
+		return (float) color.getAlpha() / 255f;
+	}
+
+	public static float[] convertToFME(Color color) {
+		return new float[]{
+			getRed(color),
+			getGreen(color),
+			getBlue(color),
+			getAlpha(color)
+		};
+	}
+
+}
